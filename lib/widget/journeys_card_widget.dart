212115e14c7b2e80aa3289e4b5b5555a1c56271f
Matvey0808 <avto.mat.2000g@gmail.com>
2026-04-24 15:00:52 +0700

    feat: add GestureDetector onLongPress
    - delete floatingActionButton dialogEdit from JourneysView
    - add onLongPress on CardJourneys

--- lib/widget/journeys_card_widget.dart
+++ lib/widget/journeys_card_widget.dart
@@ -1,3 +1,4 @@
+import 'package:bonbagage/widget/dialog_edit_widget.dart';
 import 'package:flutter/material.dart';
 
 class CardJourneys extends StatelessWidget {
@@ -11,30 +12,35 @@ class CardJourneys extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final isTable = width >= 600;
-    return Card(
-      color: Color(0xFFf2f2f2),
-      child: SizedBox(
-        height: isTable ? width * 0.1 : width * 0.2,
-        child: Row(
-          children: [
-            SizedBox(width: 10),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                title,
-                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+    return GestureDetector(
+      onLongPress: () {
+        journeyEditDialog(context);
+      },
+      child: Card(
+        color: Color(0xFFf2f2f2),
+        child: SizedBox(
+          height: isTable ? width * 0.1 : width * 0.2,
+          child: Row(
+            children: [
+              SizedBox(width: 10),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  title,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                ),
               ),
-            ),
-            const Spacer(),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Text(
-                "$startDate - $endDate",
-                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              const Spacer(),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  "$startDate - $endDate",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                ),
               ),
-            ),
-            SizedBox(width: 10),
-          ],
+              SizedBox(width: 10),
+            ],
+          ),
         ),
       ),
     );
